501815e9fd6b619a0055cd7f9afa339ec16880c1
Benjamin Nørgaard <b@noergaard.dk>
2016-09-08 22:34:47 +0200

    Add tests for MailingProviders

--- mailspree_test.go
+++ mailspree_test.go
@@ -1,7 +1,33 @@
 package mailspree_test
 
-import "testing"
+import (
+	"testing"
 
-func TestFailure(t *testing.T) {
-	// t.Error("failure")
+	"github.com/blacksails/mailspree"
+	"github.com/blacksails/mailspree/mock"
+)
+
+func TestMailingProviders(t *testing.T) {
+	// Positive case
+	mp1 := mock.FailingMailingProvider{}
+	mp2 := &mock.SucceedingMailingProvider{}
+	mp3 := &mock.SucceedingMailingProvider{}
+	mps := mailspree.MailingProviders{mp1, mp2, mp3}
+	err := mps.SendEmail(mailspree.Email{})
+	if err != nil {
+		t.Error("the second provider should cause err to eq nil")
+	}
+	if len(mp2.SentEmail) != 1 {
+		t.Error("the second provider should send the email")
+	}
+	if len(mp3.SentEmail) != 0 {
+		t.Error("the third provider should not send the email")
+	}
+
+	// Negative case
+	mps = mailspree.MailingProviders{mp1, mp1, mp1}
+	err = mps.SendEmail(mailspree.Email{})
+	if err == nil {
+		t.Error("if all mailing providers fail we should return an error")
+	}
 }
