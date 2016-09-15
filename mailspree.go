7f772215ebf82f9859a7a931d8d8145bfa1ac9a4
Benjamin Nørgaard <b@noergaard.dk>
2016-09-15 23:43:37 +0200

    Add documentation for the main mailspree package

---
+++ mailspree.go
@@ -0,0 +1,4 @@
+// Package mailspree contains all of the mailspree domain models and interfaces.
+// Implementation of interfaces is generally put into subpackages named by the
+// main dependency which that particular implementaion is based upon.
+package mailspree
