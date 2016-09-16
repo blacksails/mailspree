adc321c563c145c9d42d0bf0574fab9cbe7c921b
Benjamin Nørgaard <b@noergaard.dk>
2016-09-16 13:10:21 +0200

    Add empty tests to have packages listed in coverage

---
+++ cmd/mailspree/main_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestFunction(t *testing.T) {
+	// This test is here just to generate coverage of the package
+}
---
+++ jwt/auth_service_test.go
@@ -0,0 +1,7 @@
+package jwt_test
+
+import "testing"
+
+func TestFunction(t *testing.T) {
+	// This test is here just to generate coverage of the package
+}
